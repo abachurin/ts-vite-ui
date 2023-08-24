be10555b2dd875a521436c7a0a4f97d79aca6b83
Alex Bachurin <bachurin.alex@gmail.com>
2023-08-24 22:18:40 +0100

    *

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
-import dns from "dns";
 import react from "@vitejs/plugin-react";
-
-dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
         target: "esnext",
-    },
-    server: {
-        host: "0.0.0.0",
-        port: 8080,
     },
 });
