f2805ca2d878a3e2886ca60e5475465e47ca9629
Alex Bachurin <bachurin.alex@gmail.com>
2023-08-26 08:36:10 +0100

    *

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const BACK_URL =
     process.env.NODE_ENV === "development"
         ? "http://localhost:5000"
-        : "http://api.robot2048.com:5000";
+        : "https://api.robot2048.com:5000";
